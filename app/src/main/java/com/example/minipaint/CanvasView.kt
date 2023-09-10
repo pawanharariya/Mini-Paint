8c8fe75eff18b78a8475b1cf8f5524b5953912e6
pawanharariya <pawanharariya@gmail.com>
2023-09-10 23:49:17 +0530

    Add drawing support to canvas

--- app/src/main/java/com/example/minipaint/CanvasView.kt
+++ app/src/main/java/com/example/minipaint/CanvasView.kt
@@ -1,32 +1,108 @@
 package com.example.minipaint
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.abs
+
+private const val STROKE_WIDTH = 12f
 
 class CanvasView(context: Context) : View(context) {
 
-    // Canvas and Bitmap for caching what has drawn before
-    private lateinit var extraCanvas: Canvas
-    private lateinit var extraBitmap: Bitmap
+    // Path representing the drawing so far
+    private val drawing = Path()
+
+    // Path representing what is currently being drawn
+    private val curPath = Path()
 
     private val backgroundColor =
         ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+    private var currentX = 0f
+    private var currentY = 0f
+
+    /**
+     *
+     * There is not need to draw every pixel and each time request a refresh of the display
+     * Instead draw a path between points, for better performance
+     * If the finger is barely moved or moved less than tolerance distance don't draw
+     */
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.SQUARE // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        // new bitmap and canvas is created everytime when size changes, since the old bitmap
-        // is around it is a memory leak, so recycle extra bitmap before creating next one
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
-        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+        // Draw the drawing so far
+        canvas?.drawPath(drawing, paint)
+        canvas?.drawPath(curPath, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private fun touchUp() {
+        // Add the current path to the drawing so far
+        drawing.addPath(curPath)
+        // Rewind the current path for the next touch
+        curPath.reset()
+    }
+
+    private fun touchMove() {
+        // Calculate the distance moved by the finger
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            curPath.quadTo(
+                currentX,
+                currentY,
+                (motionTouchEventX + currentX) / 2,
+                (motionTouchEventY + currentY) / 2
+            )
+            // Reset the end point to current point
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+        }
+        invalidate()
+    }
+
+    private fun touchStart() {
+        curPath.reset()
+        curPath.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
     }
 }
